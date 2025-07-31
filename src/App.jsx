84ec3aa5f23d3008ad3d9614fc814873c9766376
Satyamrk <satyamrk18@gmail.com>
2025-07-31 13:42:49 +0530

    navbar routes aded

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        {/* <Routes path="/carrer" element={<Carrer />}></Routes>
+        <Routes path="/courses" element={<Courses />}></Routes>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/login" element={<Login />}></Route> */}
       </Routes>
       </BrowserRouter>
     </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,8 +3,12 @@ import {Link, useLocation} from "react-router-dom"
 import "./navbar.css"
 const navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar'>
+     <Link to="/" >Home</Link>
+     <Link to="/carrer">Carrer</Link>
+     <Link to="/courses">courses</Link>
+     <Link to="/about">about</Link>
+     <Link to="/login">login</Link>
     </div>
   )
 }
