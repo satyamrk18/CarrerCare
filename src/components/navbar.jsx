184e1145b99dc898542c3b7e720f5f1df082be85
Satyamrk <satyamrk18@gmail.com>
2025-07-31 14:41:52 +0530

    simple styling added to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 import {Link, useLocation} from "react-router-dom"
 import "./navbar.css"
 const navbar = () => {
+    const location  = useLocation();
   return (
     <div className='navbar'>
-     <Link to="/" >Home</Link>
-     <Link to="/carrer">Carrer</Link>
-     <Link to="/courses">courses</Link>
-     <Link to="/about">about</Link>
-     <Link to="/login">login</Link>
+       <h2 className='logo'>Logo</h2>
+   <div className='navbar-content'>
+      <Link to="/" className={location.pathname === "/"? "active": "navbar-item"}>Home</Link>
+     <Link to="/carrer" className={location.pathname === "/carre"? "active": "navbar-item"}>Carrer</Link>
+     <Link to=""className={location.pathname == "/courses"? "active": "navbar-item"}>courses</Link>
+     <Link to="/about"className={location.pathname === "/about"? "active": "navbar-item"}>about</Link>
+     <Link to="/login" className={location.pathname === "/login"? "active": "navbar-item"}>login</Link>
+   </div>
     </div>
   )
 }
 
 export default navbar
+    
