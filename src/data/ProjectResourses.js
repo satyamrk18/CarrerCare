1be285206471c6fe2f2da29d184d558559276aa0
Kalme Varad Shriram <kalmevaradshriram@Kalmes-MacBook-Air.local>
2025-08-02 16:44:17 +0530

    web devlopment projects added

---
+++ src/data/ProjectResourses.js
@@ -0,0 +1,45 @@
+const projects = [
+  // Web Development Projects
+  {
+    projectName: "Event Webpage",
+    description: "Build a responsive event landing page using HTML, CSS, and basic JavaScript to display event info and collect signups.",
+    resources: [
+      "https://www.geeksforgeeks.org/web-development-projects/"
+    ],
+    category: "Web Development"
+  },
+  {
+    projectName: "Personal Blog",
+    description: "Create a personal blog with posts, categories, and SEO-friendly routing using frontend frameworks or plain HTML/CSS.",
+    resources: [
+      "https://www.odinschool.com/blog/best-web-development-project-ideas-beginners-to-advance"
+    ],
+    category: "Web Development"
+  },
+  {
+    projectName: "Social Media Dashboard",
+    description: "Design an interactive dashboard to display user stats like likes, followers, and shares using APIs and data visualization.",
+    resources: [
+      "https://www.odinschool.com/blog/best-web-development-project-ideas-beginners-to-advance"
+    ],
+    category: "Web Development"
+  },
+  {
+    projectName: "Portfolio Gallery",
+    description: "Create a flexible grid-based image or video portfolio using CSS Flexbox/Grid and JavaScript animations for transitions.",
+    resources: [
+      "https://www.geeksforgeeks.org/web-development-projects/"
+    ],
+    category: "Web Development"
+  },
+  {
+    projectName: "E-Commerce Platform",
+    description: "Build a basic e-commerce website with product listings, homepage, shopping cart, and a checkout page.",
+    resources: [
+      "https://www.odinschool.com/blog/best-web-development-project-ideas-beginners-to-advance"
+    ],
+    category: "Web Development"
+  },
+
+  
+]
