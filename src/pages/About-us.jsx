d59b5681ac37e4b0a49806552200c86c07dbfa59
Tanishka Shivarkar <tanishkas3112@gmail.com>
2025-08-07 18:23:58 +0530

    team card added

--- src/pages/About-us.jsx
+++ src/pages/About-us.jsx
@@ -18,11 +18,30 @@ const AboutUs = () => {
             </h2>
              <Button name="Explore Careers" />
            </div>
+             <div className="login-form">
+               <h2 className="login-heading">Our Team</h2>
+                <div className="team-cards">
+                   <div className="member-card">
+                       <h3>Tanishka Shivarkar</h3>
+                         <div className="social-links">
+                           <a href="https://github.com/TanishkaShivarkar" target="_blank">
+                           <Github size={20} />
+                           </a>
+                            <a href="https://www.linkedin.com/in/tanishka-shivarkar-566351375/" target="_blank">
+                           <Linkedin size={20} />
+                           </a>
+                            <a href="https://peerlist.io/tanishka_3112" target="_blank">
+                           <Peerlist size={20} />
+                            </a>
+                         </div>
+                   </div>
+                </div>
+             </div>
         </div>
       </div>
     </div>
     
-  )
-}
+  );
+};
 
-export default AboutUs
+export default AboutUs;
