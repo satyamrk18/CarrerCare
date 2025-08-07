6d01b166dafb54a8035feb1462ad892fa9180d6a
anshuman morey <anshumanmorey2@gmail.com>
2025-08-07 21:22:22 +0530

    add more tailwindcss for mobile view

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
     <>
       <div className="bg-gray-100">
        <div className=" flex justify-center items-center w-full min-h-screen"> 
-          <div className=" bg-cyan-700 w-full max-w-4xl p-8 rounded-xl shadow-lg text-white">
+          <div className=" flex flex-col space-y-6 bg-cyan-700 w-full max-w-4xl p-8 rounded-xl shadow-lg text-white">
 
           </div>
        </div>
