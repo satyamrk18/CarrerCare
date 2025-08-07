494cde1866792bc4ae7be03eca7e13c4c6419a2e
anshuman morey <anshumanmorey2@gmail.com>
2025-08-07 22:08:46 +0530

    add replicas of contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -15,8 +15,16 @@ const Contact = () => {
                     
                   </p>
                </div>
-               <div>
+               <div className="flex flex-col space-y-4">
                 <div className="inline-flex space-x-2 items-center">
+                    <span className="text-cyan-300">contact.jpg</span> 
+                   <span> (+91) 8142 6243 12 </span>
+                </div>  
+                <div className="inline-flex space-x-2 items-center">
+                    <span className="text-cyan-300">contact.jpg</span> 
+                   <span> (+91) 8142 6243 12 </span>
+                </div>
+                  <div className="inline-flex space-x-2 items-center">
                     <span className="text-cyan-300">contact.jpg</span> 
                    <span> (+91) 8142 6243 12 </span>
                 </div>
