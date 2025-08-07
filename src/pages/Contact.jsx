33795fb934c6fb46a70adf0b43fd60082c9a19db
anshuman morey <anshumanmorey2@gmail.com>
2025-08-07 22:04:30 +0530

    add contact and jpg will be added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,18 +8,28 @@ const Contact = () => {
       <div className="bg-gray-100">
        <div className=" flex justify-center items-center w-full min-h-screen"> 
           <div className=" flex flex-col space-y-6 bg-cyan-700 w-full max-w-4xl p-8 rounded-xl shadow-lg text-white">
-            <div className="flex flex-col justify-between"> 
+            <div className="flex flex-col space-y-8 justify-between"> 
                <div>
                  <h1 className=" font-bold text-4xl tracking-wide">contact us </h1>
-                  <p className="pt-2 text-cyan-100 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, minus.</p>
+                  <p className="pt-2 text-cyan-100 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, minus.
+                    
+                  </p>
                </div>
-               <div></div>
+               <div>
+                <div className="inline-flex space-x-2 items-center">
+                    <span className="text-cyan-300">contact.jpg</span> 
+                   <span> (+91) 8142 6243 12 </span>
+                </div>
+               
+               </div>
                <div></div>
             </div> 
             <div></div>  
           </div>
        </div>
       </div>
+      
+     
 
     </>
   )
