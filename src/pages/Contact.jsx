d80d5e9f2884f380710b12814732b72684cab238
anshuman morey <anshumanmorey2@gmail.com>
2025-08-07 21:29:22 +0530

    add contact us and paragraph

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,7 +8,15 @@ const Contact = () => {
       <div className="bg-gray-100">
        <div className=" flex justify-center items-center w-full min-h-screen"> 
           <div className=" flex flex-col space-y-6 bg-cyan-700 w-full max-w-4xl p-8 rounded-xl shadow-lg text-white">
-
+            <div className="flex flex-col justify-between"> 
+               <div>
+                 <h1 >contact us </h1>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, minus.</p>
+               </div>
+               <div></div>
+               <div></div>
+            </div> 
+            <div></div>  
           </div>
        </div>
       </div>
