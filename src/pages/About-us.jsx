5e94494bbcde2ecf9d9b3d45175444d755f10e14
Tanishka Shivarkar <tanishkas3112@gmail.com>
2025-08-07 18:14:17 +0530

    summary added

--- src/pages/About-us.jsx
+++ src/pages/About-us.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import './About-us.css'
+import Navbar from './../components/navbar.jsx'
 
-function AboutUs() {
+const AboutUs = () => {
   return (
-    <div>About Us</div>
+    <div>
+      <Navbar />
+      <div className="conatiner">
+        <div className="login-container">
+           <div className="side-container"> 
+             <h1>About CareerCare</h1>
+             <h2>
+              CareerCare is a career exploration platform that helps students and learners
+              discover different career options, required skills, courses, job roles, and
+              growth paths. It aims to make career planning easy, informative, and
+              engaging.
+            </h2>
+             <Button name="Explore Careers" />
+           </div>
+        </div>
+      </div>
+    </div>
+    
   )
 }
 
