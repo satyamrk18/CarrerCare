c0e9daaea0f3bc5f8ad627c7c970e212248118f3
Kalme Varad Shriram <kalmevaradshriram@Kalmes-MacBook-Air.local>
2025-08-09 14:24:47 +0530

    more rojects are added

---
+++ src/pages/Home 2.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Navbar from './../components/navbar.jsx'
+import "./Home.css"
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+export default Home
