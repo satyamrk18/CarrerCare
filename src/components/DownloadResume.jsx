62771d9d35caa140250aca94dea685600bee881a
Satyamrk <satyamrk18@gmail.com>
2025-08-11 14:55:50 +0530

    resume download comple

--- src/components/DownloadResume.jsx
+++ src/components/DownloadResume.jsx
@@ -1,40 +1,29 @@
-import domtoimage from "dom-to-image-more";
-import "./DownloadResume.css"
-import { ImageDown } from 'lucide-react';
-const DownloadButton = ({ targetRef }) => {
-  const downloadImage = async () => {
-    const node = targetRef.current;
+const downloadImage = async () => {
+  const node = targetRef.current;
 
-    const scale = 3; // 🔍 increase scale for sharpness
-    const style = getComputedStyle(node);
-    const width = node.offsetWidth * scale;
-    const height = node.offsetHeight * scale;
+  const scale = 3;
+  const width = node.offsetWidth * scale;
+  const height = node.offsetHeight * scale;
 
-    const clonedStyle = {
-      transform: `scale(${scale})`,
-      transformOrigin: "top left",
-      width: `${node.offsetWidth}px`,
-      height: `${node.offsetHeight}px`,
-    };
-
-    domtoimage
-      .toPng(node, {
-        width,
-        height,
-        style: clonedStyle,
-      })
-      .then((dataUrl) => {
-        const link = document.createElement("a");
-        link.download = "emoji.png";
-        link.href = dataUrl;
-        link.click();
-      })
-      .catch((err) => {
-        console.error("Failed to download image", err);
-      });
-  };
-
-  return <button className="download-btn" onClick={downloadImage}>Download<ImageDown /></button>;
+  domtoimage
+    .toPng(node, {
+      width,
+      height,
+      style: {
+        transform: `scale(${scale})`,
+        transformOrigin: "top left",
+        width: `${node.offsetWidth}px`,
+        height: `${node.offsetHeight}px`,
+      },
+      backgroundColor: "#ffffff",
+    })
+    .then((dataUrl) => {
+      const link = document.createElement("a");
+      link.download = "resume.jpg";
+      link.href = dataUrl;
+      link.click();
+    })
+    .catch((err) => {
+      console.error("Failed to download image", err);
+    });
 };
-
-export default DownloadButton;
