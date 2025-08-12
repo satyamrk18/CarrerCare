6bc26d08c6575d6521f0db436311dddec92d1aa9
Satyamrk <satyamrk18@gmail.com>
2025-08-12 20:34:23 +0530

    local image integration with user profilr

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -3,9 +3,13 @@ import { useState, useEffect } from "react";
 import Navbar from "./../components/navbar.jsx";
 import { Link } from "react-router-dom";
 import "./user.css";
-import Button from "../components/button.jsx"
-const user = () => {
+import Button from "../components/button.jsx";
+import Input from "../components/input.jsx";
+
+
+const User = () => {
   const [user, setUser] = useState("");
+  const [userImg, setUserImg] = useState("");
   //retriving the data if exist
   useEffect(() => {
     const data = JSON.parse(localStorage.getItem("userdata") || "null");
@@ -17,17 +21,36 @@ const user = () => {
   return (
     <div>
       <Navbar />
-     <div  className="user-container">
-       <div className="userData">
-        <h1>hello , {name}</h1>
+      <div className="user-container">
+        <div className="userData">
+         <div className="userImage">
+           <img src={userImg} alt="user image"/>
+         </div>
+          <Input
+            type="file"
+            heading="upload the image"
+            placeholder="upload the image"
+            onChange={(e)=>{
+              const file = e.target.files[0];
+              if(file)
+              {
+                setUserImg(URL.createObjectURL(file));
+              }
+            }}
+          />
+          <h1>hello , {name}</h1>
+        </div>
+        <div className="user-Suggessions">
+          <Link to="/contact">
+            <Button name="Contact" />
+          </Link>
+          <Link to="/resume">
+            <Button name="Resume" />
+          </Link>
+        </div>
       </div>
-      <div className="user-Suggessions">
-        <Link to="/contact"><Button name="Contact"/></Link>
-        <Link to="/resume"><Button name="Resume"/></Link>
-      </div>
-     </div>
     </div>
   );
 };
 
-export default user;
+export default User;
