88e33eadb671570c35477cb2d0c5a4e777e0176b
Satyamrk <satyamrk18@gmail.com>
2025-08-12 20:20:33 +0530

    contact and resume button style change in user page

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,28 +1,33 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import Navbar from "./../components/navbar.jsx"
-import {Link} from "react-router-dom"
-import "./user.css"
+import React from "react";
+import { useState, useEffect } from "react";
+import Navbar from "./../components/navbar.jsx";
+import { Link } from "react-router-dom";
+import "./user.css";
+import Button from "../components/button.jsx"
 const user = () => {
- 
-const [user,setUser] = useState("")
-//retriving the data if exist
-useEffect(()=>
-{
- const data = JSON.parse(localStorage.getItem("userdata") || "null");
-  setUser(data);
-},[])
+  const [user, setUser] = useState("");
+  //retriving the data if exist
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("userdata") || "null");
+    setUser(data);
+  }, []);
 
-const {name} = user;
+  const { name } = user;
 
   return (
-    <div className='user-container'>
-        <Navbar />
-      <h1>hi, {user.name}</h1>
-      <Link to="/contact">Contact</Link> 
-      <Link to="/resume">Resume</Link>
+    <div>
+      <Navbar />
+     <div  className="user-container">
+       <div className="userData">
+        <h1>hello , {name}</h1>
+      </div>
+      <div className="user-Suggessions">
+        <Link to="/contact"><Button name="Contact"/></Link>
+        <Link to="/resume"><Button name="Resume"/></Link>
+      </div>
+     </div>
     </div>
-  )
-}
+  );
+};
 
-export default user
+export default user;
