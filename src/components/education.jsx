8a422530837f50796f9e437ae060a0bd9a61fa82
Satyamrk <satyamrk18@gmail.com>
2025-08-13 14:33:13 +0530

    education css convert into tailwind css

--- src/components/education.jsx
+++ src/components/education.jsx
@@ -1,14 +1,16 @@
 import "./education.css"
 const Education = ({education,college,percentage})=>
 {
- return(
-    <div className="education-comp">
-    <ul>
-        <li>{education} </li>
-        <li>{college}</li>
-        <li>percentage : {percentage}%</li>
-    </ul>
+  return (
+    <div className="break-words max-w-full">
+      <ul className="list-none p-0 m-0">
+        <li className="break-words whitespace-pre-wrap mb-[5px]">{education}</li>
+        <li className="break-words whitespace-pre-wrap mb-[5px]">{college}</li>
+        <li className="break-words whitespace-pre-wrap mb-[5px]">
+          percentage : {percentage}%
+        </li>
+      </ul>
     </div>
- )
+  );
 }
 export default Education;
