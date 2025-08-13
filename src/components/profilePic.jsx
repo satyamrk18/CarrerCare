d867034587db7b9d767e38080b9e7c55b56d100d
Satyamrk <satyamrk18@gmail.com>
2025-08-13 14:40:18 +0530

    profile css convert it into tailwind css

--- src/components/profilePic.jsx
+++ src/components/profilePic.jsx
@@ -1,9 +1,12 @@
-import "./profilePic.css";
 import DefaultProfile from "../assets/img/defaultProfilePic.jpg";
 const profile = ({ profilepic }) => {
   return (
     <div className="default-Profile-Pic">
-      <img src={profilepic === "" ? DefaultProfile : profilepic} />
+      <img
+        src={profilepic === "" ? DefaultProfile : profilepic}
+        ale="profile pic"
+        className="rounded-full w-[150px] h-[150px] border border-gray-500"
+      />
     </div>
   );
 };
